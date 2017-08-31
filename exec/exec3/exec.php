77069a6c4d02c61e9fd0ae11aadc0abe097a6cb5
Image <malingtao1019@163.com>
2017-08-31 21:09:59 +0800

    Update exec.php
    Added show_source

--- exec/exec3/exec.php
+++ exec/exec3/exec.php
@@ -1,6 +1,11 @@
 <?php
-if(strlen($_GET[1])<8){
-     echo shell_exec($_GET[1]);
+if(isset($_GET[1])){
+     if(strlen($_GET[1])<8){
+          echo shell_exec($_GET[1]);
+     }
+}
+else{
+     show_source(__FILE__);
 }
 
 ?>
