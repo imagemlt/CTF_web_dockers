cb6416446de03f86d99ce1b13d9a950be4e9f299
imagemlt <malingtao1019@163.com>
2017-09-01 13:12:46 +0800

    adjusted

--- shiyanbar/web_collection_1/web3/flag.php
+++ shiyanbar/web_collection_1/web3/flag.php
@@ -1,2 +1,4 @@
 <?php
+$username="afaferwersagasgsgsa";
+$password="agagwwgrgawgwgw";
 $flag="CTF{this_is_flag_web3}";
--- shiyanbar/web_collection_1/web3/web3.php
+++ shiyanbar/web_collection_1/web3/web3.php
@@ -2,7 +2,7 @@
 show_source(__FILE__);
 $username  = "this_is_secret"; 
 $password  = "this_is_not_known_to_you"; 
-include("flag.php");
+include("flag.php");//here I changed those two 
 $info = isset($_GET['info'])? $_GET['info']: "" ;
 $data_unserialize = unserialize($info);
 if ($data_unserialize['username']==$username&&$data_unserialize['password']==$password){
