0aea1af5e46a6dd91c37504069013c9787d18e5a
Image <malingtao1019@163.com>
2017-09-02 01:12:00 +0800

    Update database.sql

--- php4fun/challenge9/database.sql
+++ php4fun/challenge9/database.sql
@@ -7,10 +7,10 @@ writer varchar(255) not null,
 idx int auto_increment,
 primary key(idx)
 );
-CREATE TABLE flag{
+CREATE TABLE flag(
 flag varchar(255) not null,
 id int auto_increment,
 primary key(id)
-}
+);
 INSERT INTO board(subject,content,writer) VALUES('math','qwesifubaoausfbiaaf','xu'),('english','adasdbasawerwerwebsd','Raphael'),('chinese','qqqq','image');
 INSERT INTO flag(flag) values('CTF{5ql1_upp3rc4s3}');
