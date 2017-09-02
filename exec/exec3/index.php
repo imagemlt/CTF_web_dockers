076f41e51364eb202b2edf684c8377dcdaaaa36a
imagemlt <malingtao1019@163.com>
2017-09-02 01:27:17 -0400

    adjusted

---
+++ exec/exec3/index.php
@@ -0,0 +1,2 @@
+<?php
+header('Location: http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].'/exec3.php');
--- php4fun/challenge8/index.php
+++ php4fun/challenge8/index.php
@@ -1,2 +1,2 @@
 <?php
-header('Location: http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].$_SERVER["REQUEST_URI"].substr($_SERVER["REQUEST_URI"],1,strlen($_SERVER['REQUEST_URI'])-2).'.php');
+header('Location: http://'.$_SERVER['SERVER_NAME'].':'.$_SERVER["SERVER_PORT"].'/challenge8.php');
